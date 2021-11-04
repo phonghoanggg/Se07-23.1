838ebf0c28fa3c418f604fc5a6cbfe274beb2440
XuanThuong01 <phamxuanthuong2301@gmail.com>
2021-11-04 22:56:21 +0700

    tach view voi controller

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,9 @@
 import express from "express";
+import homeController from "../controllers/HomeController";
 
 let router = express.Router();
 let initWebRoutes = (app) => {
-    router.get("/", (req, res) => {
-        return res.send("hello");
-    });
+    router.get("/", homeController.getHomePage);
     return app.use("/", router);
 };
 module.exports = initWebRoutes;
