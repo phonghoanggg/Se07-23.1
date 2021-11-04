9d0731d17ad54b6e673bf00ca011db052ff25b6c
XuanThuong01 <phamxuanthuong2301@gmail.com>
2021-11-04 23:15:23 +0700

    trien khai len heroku

--- src/routes/web.js
+++ src/routes/web.js
@@ -4,6 +4,8 @@ import homeController from "../controllers/HomeController";
 let router = express.Router();
 let initWebRoutes = (app) => {
     router.get("/", homeController.getHomePage);
+    router.get("/webhook", homeController.getWebHook);
+    router.post("/webhook", homeController.postWebHook);
     return app.use("/", router);
 };
 module.exports = initWebRoutes;
